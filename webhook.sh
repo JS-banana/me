ee343191cf593abf371c1084055cb3e6a0de7f26
sunss <sss213018@163.com>
2021-01-19 23:40:51 +0800

    ✅ 测试 脚本

--- webhook.sh
+++ webhook.sh
@@ -1,4 +1,20 @@
+# ! /bin/bash
+# 确保脚本抛出遇到的错误
+set -e
 
+echo 'start===>git'
+
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+
+echo 'end===>git'
+
+echo 'start===>create webhook.sh'
+
+# 生成webhook.sh文件 fix线上更新后无文件
+cat>webhook.sh<<EOF
 # ! /bin/bash
 # 确保脚本抛出遇到的错误
 set -e
@@ -7,3 +23,6 @@ git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
+EOF
+
+echo 'end===>create webhook.sh'
