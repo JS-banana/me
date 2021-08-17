05aa27f57a70d23ed25a908f22f54dab7b2f8766
sunss <sss213018@163.com>
2021-08-17 22:01:55 +0800

    ✅ workflow：coding.sh

--- coding.sh
+++ coding.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# 该命令是在 hexo 部署之后才会执行，即github pages已被创建了
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -9,7 +11,8 @@ echo '部署到coding====>开始...'
 nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 
 # 进入生成的文件夹
-cd ./.deploy_git
+# cd ./.deploy_git
+cd ./public
 
 # var
 GH_REF="e.coding.net/ssscode/blog/hexo.git"
@@ -17,12 +20,15 @@ nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 
 # echo 'me.ssscode.com' > CNAME  # 自定义域名
 
+git config --global user.name "JS-banana"
+git config --global user.email "sss213018@163.com"
+
 git init
 git add -A
 git commit -m "coding.sh===>update：${nowDate}"
 git push -f "https://ptzv1yuleer1:${CODING_TOKEN}@${GH_REF}" master:master # 推送到 coding 
 
 cd - # 退回开始所在目录
-rm -rf .deploy_git
+# rm -rf .deploy_git
 
 echo '部署到coding====>完成'
