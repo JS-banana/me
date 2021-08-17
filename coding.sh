89621bbb3b8f0a0c72ace588eb2a1d312cd93a94
sunss <sss213018@163.com>
2021-08-17 22:23:18 +0800

    ✅ workflow：update coding.sh

--- coding.sh
+++ coding.sh
@@ -22,15 +22,18 @@ nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 
 # echo 'me.ssscode.com' > CNAME  # 自定义域名
 
+git checkout -b coding-branch
+rm -rf .git
 git config --global user.name "JS-banana"
 git config --global user.email "sss213018@163.com"
-
-# git init
-# git add -A
-# git commit -m "coding.sh===>update：${nowDate}"
+git init
+git add -A
+git commit -m "coding.sh===>update：${nowDate}"
 git push -f "https://ptzv1yuleer1:${CODING_TOKEN}@${GH_REF}" master:master # 推送到 coding 
 
 cd - # 退回开始所在目录
 # rm -rf .deploy_git
 
 echo '部署到coding====>完成'
+
+git branch -d coding-branch
