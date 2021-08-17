75df580894c35009c3644758444f9f79edfebd18
sunss <sss213018@163.com>
2021-08-17 22:36:54 +0800

    ✅ workflow

--- coding.sh
+++ coding.sh
@@ -12,9 +12,7 @@ nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 
 # 进入生成的文件夹
 # cd ./.deploy_git
-# cd ./public
-# 直接推送至coding使用hexo环境部署
-rm -rf public .deploy_git
+cd ./public
 
 # var
 GH_REF="e.coding.net/ssscode/blog/hexo.git"
@@ -22,18 +20,15 @@ nowDate=$(date "+%Y-%m-%d %H:%M:%S")
 
 # echo 'me.ssscode.com' > CNAME  # 自定义域名
 
-git checkout -b coding-branch
-rm -rf .git
 git config --global user.name "JS-banana"
 git config --global user.email "sss213018@163.com"
+
 git init
 git add -A
 git commit -m "coding.sh===>update：${nowDate}"
 git push -f "https://ptzv1yuleer1:${CODING_TOKEN}@${GH_REF}" master:master # 推送到 coding 
 
-# cd - # 退回开始所在目录
+cd - # 退回开始所在目录
 # rm -rf .deploy_git
 
 echo '部署到coding====>完成'
-
-git branch -d coding-branch
